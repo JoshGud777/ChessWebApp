4f659292c0063b1306dda67e35d9700b67055a91
JoshuaGud777 <joshgud777@gmail.com>
2015-03-31 16:57:26 -0600

    Submit users

--- webapp/game.py
+++ webapp/game.py
@@ -4,6 +4,25 @@ if __name__ == '__main__':
 else:
     import webapp.library as lib
 
-lib.print_header()
-html = lib.get_html(lib.HTML_DIR + 'game.html')
-print(html)
+def html_print(userlist):
+    html = lib.get_html(lib.HTML_DIR + 'game.html')
+    optionlist = ''
+    for user in userlist:
+        username = user[0]
+        display = user[1]
+
+        option = '<option value="' + username + '">' + display + '</option>\n'
+        optionlist += option
+
+    html = html.replace('$$users$$$', optionlist)
+    print(html)
+
+def main():
+    conn, c = lib.open_conn(lib.DB_DIR + 'ChessApp.db')
+
+    html = lib.get_html(lib.HTML_DIR + 'game.html')
+    c.execute("SELECT username, display FROM logon")
+    dbdata = c.fetchall()
+    html_print(dbdata)
+
+    lib.close_conn(conn)
